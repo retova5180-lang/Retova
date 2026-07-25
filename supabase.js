ee27fe94ae9bfdbd5d030a664d3cd1b8a337b650
retova5180-lang <retova5180@gmail.com>
2026-07-25 04:16:42 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://bfqsqgfyyewnfxekirfv.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_iRgr41jJDJdpbHUHCY2cnA_ufe9qgkz";
+const SUPABASE_KEY = https:sb_publishable_iRgr41jJDJdpbHUHCY2cnA_ufe9qgkz";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
