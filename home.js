a229b2d5370fe857b6c2720e3c40c0196db64873
retova5180-lang <retova5180@gmail.com>
2026-07-29 09:55:59 +0300

    Update home.js

--- home.js
+++ home.js
@@ -1,168 +1,61 @@
-// =============================
-// Retova Home
-// =============================
+// =========================
+// RETOVA STORIES
+// =========================
 
-const stories = document.getElementById("stories");
-const feed = document.getElementById("feed");
+const storiesContainer = document.querySelector(".stories");
 
-const storiesData = [
+const stories = [
+
 {
 name:"You",
-avatar:"https://i.pravatar.cc/150?img=1"
+avatar:"https://i.pravatar.cc/150?img=15",
+you:true
 },
+
 {
-name:"Sarah",
-avatar:"https://i.pravatar.cc/150?img=5"
-},
-{
-name:"Liam",
-avatar:"https://i.pravatar.cc/150?img=8"
-},
-{
-name:"Emily",
-avatar:"https://i.pravatar.cc/150?img=12"
-},
-{
-name:"Olivia",
+name:"Sophia",
 avatar:"https://i.pravatar.cc/150?img=32"
 },
+
 {
-name:"Carlos",
-avatar:"https://i.pravatar.cc/150?img=18"
+name:"Noah",
+avatar:"https://i.pravatar.cc/150?img=12"
+},
+
+{
+name:"Emma",
+avatar:"https://i.pravatar.cc/150?img=48"
+},
+
+{
+name:"Liam",
+avatar:"https://i.pravatar.cc/150?img=60"
+},
+
+{
+name:"Mia",
+avatar:"https://i.pravatar.cc/150?img=24"
 }
+
 ];
 
-storiesData.forEach(item=>{
+stories.forEach(story=>{
 
-stories.innerHTML+=`
+storiesContainer.innerHTML += `
 
-<div class="story">
+<div class="story ${story.you ? "you" : ""}">
 
 <div class="story-avatar">
 
-<img src="${item.avatar}" alt="">
+<img src="${story.avatar}" alt="${story.name}">
+
+${story.you ? '<div class="add-story">+</div>' : ''}
 
 </div>
 
 <div class="story-name">
 
-${item.name}
-
-</div>
-
-</div>
-
-`;
-
-});
-const posts = [
-
-{
-name:"Sarah",
-username:"@sarah",
-avatar:"https://i.pravatar.cc/150?img=5",
-text:"Good morning everyone ☀️",
-likes:"5.2K",
-comments:"24",
-reposts:"66",
-views:"50K",
-images:[]
-},
-
-{
-name:"SALAH",
-username:"@s4u_2",
-avatar:"https://i.pravatar.cc/150?img=18",
-text:"Which destination do you like the most?",
-likes:"7.8K",
-comments:"104",
-reposts:"9",
-views:"48K",
-images:[
-"https://picsum.photos/500/500?11",
-"https://picsum.photos/500/500?12",
-"https://picsum.photos/500/500?13",
-"https://picsum.photos/500/500?14"
-]
-}
-
-];
-
-posts.forEach(post=>{
-
-let images="";
-
-if(post.images.length){
-
-images=`<div class="post-images">`;
-
-post.images.forEach(img=>{
-
-images+=`<img src="${img}">`;
-
-});
-
-images+=`</div>`;
-
-}
-
-feed.innerHTML+=`
-
-<div class="post">
-
-<div class="post-header">
-
-<div class="post-user">
-
-<img src="${post.avatar}">
-
-<div>
-
-<div class="post-name">${post.name}</div>
-
-<div class="post-username">${post.username}</div>
-
-</div>
-
-</div>
-
-<i class="fa-solid fa-ellipsis"></i>
-
-</div>
-
-<div class="post-text">
-
-${post.text}
-
-</div>
-
-${images}
-
-<div class="post-actions">
-
-<div class="action">
-<i class="fa-regular fa-comment"></i>
-<span>${post.comments}</span>
-</div>
-
-<div class="action">
-<i class="fa-solid fa-repeat"></i>
-<span>${post.reposts}</span>
-</div>
-
-<div class="action">
-<i class="fa-regular fa-heart"></i>
-<span>${post.likes}</span>
-</div>
-
-<div class="action">
-<i class="fa-regular fa-eye"></i>
-<span>${post.views}</span>
-</div>
-
-<div class="action">
-<i class="fa-solid fa-arrow-up-from-bracket"></i>
-</div>
+${story.name}
 
 </div>
 
