5261fc7d5fa7cfe58ff129cfbda78fee412b6977
retova5180-lang <retova5180@gmail.com>
2026-07-31 06:33:56 +0300

    Update home.js

--- home.js
+++ home.js
@@ -1,48 +1,60 @@
-// ==============================
-// RETOVA PART 1
-// ==============================
+// ================================
+// RETOVA DATA
+// ================================
+
+const myAvatar =
+"https://i.pravatar.cc/150?img=15";
+
+document.getElementById("myAvatar").src = myAvatar;
 
 const stories = [
+
 {
 name:"You",
-avatar:"https://i.pravatar.cc/150?img=15",
+avatar:myAvatar,
 me:true
 },
+
 {
 name:"Sophia",
 avatar:"https://i.pravatar.cc/150?img=32"
 },
+
 {
 name:"Emma",
 avatar:"https://i.pravatar.cc/150?img=47"
 },
+
 {
 name:"Liam",
 avatar:"https://i.pravatar.cc/150?img=18"
 },
+
 {
 name:"Noah",
 avatar:"https://i.pravatar.cc/150?img=12"
 },
+
 {
 name:"Olivia",
 avatar:"https://i.pravatar.cc/150?img=28"
 }
+
 ];
 
-const storiesContainer = document.getElementById("stories");
+const storiesBox=document.getElementById("stories");
 
 stories.forEach(story=>{
 
-storiesContainer.innerHTML += `
+storiesBox.innerHTML+=`
 
-<div class="story ${story.me ? "you" : ""}">
+<div class="story ${story.me ? "you":""}">
 
 <div class="story-avatar">
 
-<img src="${story.avatar}" alt="">
+<img src="${story.avatar}">
 
-${story.me ? `<div class="add-story">+</div>` : ""}
+${story.me ? '<div class="add-story">+</div>' : ''}
 
 </div>
 
