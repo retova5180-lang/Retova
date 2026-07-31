b2ee1d820c07464af20e689f34f8ef3dbb6dad0b
retova5180-lang <retova5180@gmail.com>
2026-07-31 03:50:51 +0300

    Update home.js

--- home.js
+++ home.js
@@ -1,110 +1,56 @@
-const stories = [
+// ==============================
+// RETOVA PART 1
+// ==============================
 
+const stories = [
 {
 name:"You",
-avatar:"https://i.pravatar.cc/150?img=15"
+avatar:"https://i.pravatar.cc/150?img=15",
+me:true
 },
-
 {
 name:"Sophia",
 avatar:"https://i.pravatar.cc/150?img=32"
 },
-
-{
-name:"Noah",
-avatar:"https://i.pravatar.cc/150?img=12"
-},
-
 {
 name:"Emma",
 avatar:"https://i.pravatar.cc/150?img=47"
 },
-
 {
 name:"Liam",
 avatar:"https://i.pravatar.cc/150?img=18"
 },
-
 {
-name:"Mia",
-avatar:"https://i.pravatar.cc/150?img=25"
+name:"Noah",
+avatar:"https://i.pravatar.cc/150?img=12"
+},
+{
+name:"Olivia",
+avatar:"https://i.pravatar.cc/150?img=28"
 }
-
 ];
 
-const storiesBox = document.getElementById("stories");
+const storiesContainer = document.getElementById("stories");
 
-stories.forEach(user=>{
+stories.forEach(story=>{
 
-storiesBox.innerHTML += `
+storiesContainer.innerHTML += `
 
-<div class="story">
+<div class="story ${story.me ? "you" : ""}">
 
 <div class="story-avatar">
 
-<img src="${user.avatar}">
+<img src="${story.avatar}" alt="">
+
+${story.me ? `<div class="add-story">+</div>` : ""}
 
 </div>
 
 <div class="story-name">
 
-${user.name}
+${story.name}
 
 </div>
-
-</div>
-
-`;
-
-});
-
-const posts=[
-
-{
-
-name:"Sophia",
-
-user:"@sophia",
-
-avatar:"https://i.pravatar.cc/150?img=32",
-
-text:"Good morning ☀️",
-
-image:"https://picsum.photos/700/500?random=1"
-
-},
-
-{
-
-name:"Liam",
-
-user:"@liam",
-
-avatar:"https://i.pravatar.cc/150?img=18",
-
-text:"Weekend vibes 🌴",
-
-image:"https://picsum.photos/700/500?random=2"
-
-}
-
-];
-
-const feed=document.getElementById("feed");
-
-posts.forEach(post=>{
-
-feed.innerHTML+=`
-
-<div class="post">
-
-<h3>${post.name}</h3>
-
-<p>${post.user}</p>
-
-<p>${post.text}</p>
-
-<img src="${post.image}">
 
 </div>
 
