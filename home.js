01acb1f689f047e9db94bf8c646cdd98c0394f5f
retova5180-lang <retova5180@gmail.com>
2026-08-02 11:36:51 +0300

    Update home.js

--- home.js
+++ home.js
@@ -28,3 +28,57 @@ Retova
 <section id="feed"></section>
 
 `;
+const stories=[
+
+{
+name:"You",
+img:"https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=300&fit=crop&crop=faces"
+},
+
+{
+name:"Sophia",
+img:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=300&fit=crop&crop=faces"
+},
+
+{
+name:"Noah",
+img:"https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=300&fit=crop&crop=faces"
+},
+
+{
+name:"Emma",
+img:"https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=300&fit=crop&crop=faces"
+},
+
+{
+name:"Liam",
+img:"https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=300&fit=crop&crop=faces"
+}
+
+];
+
+const storiesContainer=document.getElementById("stories");
+
+stories.forEach(story=>{
+
+storiesContainer.innerHTML+=`
+
+<div class="story">
+
+<div class="story-ring">
+
+<img src="${story.img}">
+
+</div>
+
+<div class="story-name">
+
+${story.name}
+
+</div>
+
+</div>
+
+`;
+
+});
